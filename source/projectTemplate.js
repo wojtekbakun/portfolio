de1ca2ea8e7fb214f45f19524e53df9c62538021
wojtekbakun <wojciechzsee@gmail.com>
2024-09-09 19:28:30 +0200

    added new projects and assets

--- source/projectTemplate.js
+++ source/projectTemplate.js
@@ -8,7 +8,7 @@ function setProjectTemplate(project, isHrToDisplay) {
       <div class="projects-images-box">
         <img
           class="projects-images"
-          src="assets/images/frame1.png"
+          src="${project.image}"
           alt=""
         />
       </div>
@@ -18,20 +18,7 @@ function setProjectTemplate(project, isHrToDisplay) {
           ${project.shortDescription}
         </p>
         <div class="projects-technologies">
-          <div class="image-div-border">
-            <img
-              class="tech-image"
-              src="assets/images/flutter.png"
-              alt="Flutter"
-            />
-          </div>
-          <div class="image-div-border">
-            <img
-              class="tech-image"
-              src="assets/images/firebase.png"
-              alt="Firebase"
-            />
-          </div>
+          ${getTechnologiesImages(project.technologies)}
         </div>
       </div>
     </div>
@@ -39,6 +26,67 @@ function setProjectTemplate(project, isHrToDisplay) {
   </section>`;
   
     return projectTemplate;
+  };
+
+  function getTechnologiesImages(technologies) {
+    let imagesHTML = "";
+    technologies.forEach(element => {
+     imagesHTML += `
+      <div class="image-div-border">
+            <img
+              class="tech-image"
+              src="${getSingleImage(element)}"
+              alt="Flutter"
+            />
+          </div>
+          `
+    });
+    console.log(imagesHTML);
+    return imagesHTML;
+  };
+
+  function getSingleImage(technology) {
+    console.log(technology);
+    let imageURL = "";
+    switch (technology) {
+      case "flutter":
+        imageURL = '../assets/images/flutter.png';
+        break;
+      case "firebase":
+        imageURL = '../assets/images/firebase.png';
+        break;
+      case "rpi":
+        imageURL = '../assets/images/logos_raspberry-pi.png';
+        break;
+      case "stm":
+        imageURL = '../assets/images/simple-icons_stmicroelectronics.png';
+        break;
+      case "restapi":
+        imageURL = '../assets/images/mdi_api.png';
+        break;
+      case "python":
+        imageURL = '../assets/images/logos_python.png';
+        break;
+      case "nodejs":
+        imageURL = '../assets/images/logos_nodejs.png';
+        break;
+      case "html":
+        imageURL = '../assets/images/logos_html-5.png';
+        break;
+      case "css":
+        imageURL = '../assets/images/logos_css-3.png';
+        break;
+      case "express":
+        imageURL = '../assets/images/logos_express.png';
+        break;
+      case "gemini":
+        imageURL = '../assets/images/logos_google-gemini.png';
+        break;
+      case "c":
+        imageURL = '../assets/images/devicon_c.png';
+        break;
+    }
+    return imageURL;
   };
 
     export default setProjectTemplate;
--- source/projects.js
+++ source/projects.js
@@ -2,7 +2,7 @@ import Project from './projectModel.js';
 
 const prosta = new Project(
     "Prosta Nauka Matematyki",
-    "../assets/images/frame1.png",
+    "../assets/images/prosta.png",
     "Aplikacja mobilna dla uczniów szkoły podstawowej ułatwiająca przygotowanie do egzaminu ósmoklasisty.",
     ["flutter", "firebase"],
     ["frontend"],
@@ -11,13 +11,59 @@ const prosta = new Project(
 
 const butlometr = new Project(
     "Butlometr",
-    "../assets/images/frame1.png",
+    "../assets/images/butlometr.png",
     "Aplikacja mobilna dla nurków, która pozwala na zarządzanie butlami nurkowymi.",
-    ["flutter", "firebase"],
-    ["frontend", "iot", "fullstack"],
+    ["flutter", "firebase", "rpi", "restapi"],
+    ["iot"],
     "",
 );
 
-const allProjects = [prosta, butlometr];
+const tictactoe = new Project(
+    'Tic-Tac-Toe',
+    '../assets/images/tictactoe.png',
+    "Aplikacja mobilna umożliwiająca grę w kółko i krzyżyk.",
+    ["flutter"],
+    ["frontend"],
+    "",
+);
+
+const visualQuest = new Project(
+    'Visual Quest',
+    '../assets/images/visualquest.png',
+    "Quiz sprawdzający wiedzę z szerokiego zakresu popularnych zagadnień.",
+    ["flutter"],
+    ["frontend"],
+    "",
+);
+
+const justPlanIt = new Project(
+    'Just Plan It',
+    '../assets/images/jpi3.png',
+    "Aplikacja webowa przeznaczona do planowania długoterminowych zadań.",
+    ["nodejs", "flutter", "firebase", "restapi", "gemini"],
+    ["fullstack"],
+    "",
+);
+
+const dzienDobry = new Project(
+    'Dzień Dobry',
+    '../assets/images/dziendobry.png',
+    "System ułatwiający utrzymanie roślin i informujący o warunkach atmosferycznych w praktyczny sposób",
+    ["python", "flutter", "restapi"],
+    ["fullstack", "iot"],
+    "",
+);
+
+const subG = new Project(
+    'SubG',
+    '../assets/images/subg1.png',
+    "Płytka rozojowa na bazie Nucleo przeznaczona do praktyki z komunikacją na częstotliwości poniżej 1 GHz",
+    ["stm", "c"],
+    ["iot"],
+    "",
+);
+
+
+const allProjects = [prosta, butlometr, tictactoe, visualQuest, justPlanIt, dzienDobry, subG];
 
 export { allProjects };
